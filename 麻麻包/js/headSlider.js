5d7cd6bf6332a09e70d18b11f162d1d808db7bf1
huanghongwei <984594899@qq.com>
2016-09-07 17:40:58 +0800

    我的项目

---
+++ 麻麻包/js/headSlider.js
@@ -0,0 +1,20 @@
+
+
+	$(function(){
+
+		//控制顶部广告图的轮播
+		$('#top_slider').nivoSlider({
+
+			
+			pauseTime:3000,
+			controlNav:false,
+			directionNav:false,
+		});
+
+        $('#slider').nivoSlider({
+        	
+        	
+        });
+        
+
+	})
